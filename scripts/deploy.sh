4253a021c896851549936d572e1b770c2fb6a138
Kuldip Rathod <kuldiprathod2109@gmail.com>
2025-08-28 16:40:16 +0530

    update appspec.yml

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
-# Stop existing container
-docker stop $(docker ps -q) || true
-docker rm $(docker ps -aq) || true
+set -e
 
-# Pull latest image from ECR
+# Login to ECR
+aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 414819400975.dkr.ecr.ap-south-1.amazonaws.com
+
+# Stop and remove old container (only flask-container, not all)
+docker stop flask-container || true
+docker rm flask-container || true
+
+# Remove old image (optional, to ensure fresh pull)
+docker rmi 414819400975.dkr.ecr.ap-south-1.amazonaws.com/flask-app:latest || true
+
+# Pull latest image
 docker pull 414819400975.dkr.ecr.ap-south-1.amazonaws.com/flask-app:latest
 
-docker run -d -p 80:5000 414819400975.dkr.ecr.ap-south-1.amazonaws.com/flask-app:latest
+# Run new container
+docker run -d --name flask-container -p 80:5000 414819400975.dkr.ecr.ap-south-1.amazonaws.com/flask-app:latest
